fd8bc53dbc24b2a539c9f9bdc56804bcadefcbfa
alexvidalcor <correovidal@outlook.com>
2023-10-11 19:04:35 +0000

    moved env files

--- cdk/modules/cdk_public_env.py
+++
@@ -1,7 +0,0 @@
-# Variables that do not need to be stored as secrets and modify the behavior of the AWS CDK
-
-reusableStack = False
-execLocal = False
-showPublicIp = False
-appName = "Kaggle1"
-createSG = True
